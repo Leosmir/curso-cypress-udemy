3a132eb5b500dcc189055111c17d092617b3ca05
Leosmir <leosmir.arg@gmail.com>
2024-05-16 13:06:07 -0300

    Fix report and flakyness

--- cypress/e2e/pages/login/login.methods.js
+++ cypress/e2e/pages/login/login.methods.js
@@ -2,11 +2,13 @@ import { LoginElements } from "./login.elements";
 
 export class LoginMethods{
     static insertUsername(username){
-        LoginElements.textboxes.username.type(username)
+        LoginElements.textboxes.username.invoke('val',username)
+        //se usa invoke en lugar de Type para asegurarnos de que se inserte el texto completo
     }
 
     static insertPassword(password){
-        LoginElements.textboxes.password.type(password)
+        LoginElements.textboxes.password.invoke('val',password)
+        //se usa invoke en lugar de Type para asegurarnos de que se inserte el texto completo
     }
 
     static clickOnLoginButton(){
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,7 +4,7 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://www.demoblaze.com/')
     cy.get('#login2').click()
-    LoginMethods.login('username', 'password')
+    LoginMethods.login('username010101', 'password010101')
     cy.wait(10000)
   })
 })
