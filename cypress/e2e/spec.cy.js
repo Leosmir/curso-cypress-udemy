8435fef19531ff982dd96b5bf7aaecf4d4479f31
Leosmir <leosmir.arg@gmail.com>
2024-05-17 12:13:18 -0300

    fixes signup pages

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,10 +1,18 @@
 import { LoginMethods } from "./pages/login/login.methods"
+import { SignupMethods } from "./pages/signup/signup.methods"
 
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://www.demoblaze.com/')
     cy.get('#login2').click()
     LoginMethods.login('username010101', 'password010101')
+    cy.wait(10000)
+  })
+
+  it('signup', () => {
+    cy.visit('https://www.demoblaze.com/')
+    cy.get('#signin2').click()
+    SignupMethods.signup('Leitotest', 'Leitotest')
     cy.wait(10000)
   })
 })
